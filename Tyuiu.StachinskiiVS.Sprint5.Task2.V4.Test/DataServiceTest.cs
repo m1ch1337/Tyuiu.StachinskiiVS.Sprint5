64d6887962f56141c78327218cbe171641fd1cf2
StachinskiiVS <boss20276@gmail.com>
2026-03-22 22:43:55 +0500

    Task2

---
+++ Tyuiu.StachinskiiVS.Sprint5.Task2.V4.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.StachinskiiVS.Sprint5.Task2.V4.Lib;
+
+namespace Tyuiu.StachinskiiVS.Sprint5.Task2.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckedExistsFile()
+        {
+            DataService ds = new DataService();
+            string path = @" C:\Users\stach\source\repos\Tyuiu.StachinskiiVS.Sprint5\Tyuiu.StachinskiiVS.Sprint5.Task2.V4\bin\Debug\OutPutFileTask2.csv";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.StachinskiiVS.Sprint5.Task2.V4/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.StachinskiiVS.Sprint5.Task2.V4.Lib;
+
+int[,] mtrx = new int[3, 3] { { 5, -5, -6 }, { -5, 6, -7 }, { 7, 3, 5 } };
+int rows = mtrx.GetUpperBound(0) + 1;
+int columns = mtrx.Length / rows;
+
+DataService ds = new DataService();
+Console.Title = "Спринт #5 | Выполнил: Стачинский В.С. | ИСПБ-24-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("*Спринт #5                                                                *");
+Console.WriteLine("* Тема:                                                                   *");
+Console.WriteLine("* Задание #2                                                              *");
+Console.WriteLine("* Вариант #4                                                              *");
+Console.WriteLine("* Выполнил: Стачинский В.С. | ИСПБ-24-1                                   *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов, заполненный        *");
+Console.WriteLine("* значениями с клавиатуры. Заменить положительные элементы массива на 1,  *");
+Console.WriteLine("* отрицательные на 0. Результат сохранить в файл OutPutFileTask2.csv и    *");
+Console.WriteLine("* вывести на консоль.                                                     *");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{mtrx[i, j]} \t");
+    }
+
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+string res = ds.SaveToFileTextData(mtrx);
+Console.WriteLine("Файл: " + res);
+Console.WriteLine("Создан!");
+Console.ReadKey();
