df48fedbf29b236cccebe494cf2e0f09f0503c16
StachinskiiVS <boss20276@gmail.com>
2026-03-22 22:48:08 +0500

    Task3

---
+++ Tyuiu.StachinskiiVS.Sprint5.Task3.V10.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.StachinskiiVS.Sprint5.Task3.V10.Lib
+{
+    public class DataService : ISprint5Task3V10
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask3.bin" });
+
+            double z = -1 * Math.Pow(x, 3) + 4 * Math.Pow(x, 2) - 3 / 2.0 * x;
+            z = Math.Round(z, 3);
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+            {
+                writer.Write(BitConverter.GetBytes(z));
+            }
+
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.StachinskiiVS.Sprint5.Task3.V10.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿namespace Tyuiu.StachinskiiVS.Sprint5.Task3.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            string path = @"C:\Users\stach\source\repos\Tyuiu.StachinskiiVS.Sprint5\Tyuiu.StachinskiiVS.Sprint5.Task3.V10\bin\Debug\net8.0\OutPutFileTask3.bin";
+
+            FileInfo fileUnfo = new FileInfo(path);
+            bool fileInfo = fileUnfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileInfo);
+        }
+    }
+}
